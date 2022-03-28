4f72a6c879f8bf070da5694168173db3a021f3ec
ShyQ <bartosz.kar@wp.pl>
2022-03-28 09:20:31 +0200

    Kurwa Jeż v1.0

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ window.geometry('500x400')
 
 greeting = tk.Label(text = 'Kurwa jeż, jeż w centrum miasta')
 greeting.pack()
+button = tk.Button(text = 'Gorące jeże w okolicy')
+button.pack()
 
 window.mainloop()
