a0a93d5c06508bd19c7b79a6f843c2939e393937
ShyQ <bartosz.kar@wp.pl>
2022-03-28 09:13:34 +0200

    Kurwa Jeż v1.0

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+window = tk.Tk()
+window.title('Kurwa Jeż')
+window.geometry('500x400')
+
+greeting = tk.Label(text = 'Kurwa jeż, jeż w centrum miasta')
+greeting.pack()
+
+window.mainloop()
